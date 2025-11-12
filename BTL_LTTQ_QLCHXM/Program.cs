62f86507d623ef684fbe4dbdd04b9903dbcaa017
Vo-Thien-Linh <vothienlinh2@gmail.com>
2025-11-12 21:51:46 +0700

    Da them folder va chinh lai

--- BTL_LTTQ_QLCHXM/Program.cs
+++ BTL_LTTQ_QLCHXM/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UI.FormUI;
 
 namespace BTL_LTTQ_QLCHXM
 {
@@ -16,7 +17,7 @@ namespace BTL_LTTQ_QLCHXM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Test());
         }
     }
 }
