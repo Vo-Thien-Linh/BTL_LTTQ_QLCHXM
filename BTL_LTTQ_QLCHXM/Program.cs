9a4e46b336cbc51fb282d1dd6ad694e0b46d15ed
thanh-dat1f <vntd1f@gmail.com>
2025-11-18 11:17:45 +0700

    done Quan Ly Khach Hang

--- BTL_LTTQ_QLCHXM/Program.cs
+++ BTL_LTTQ_QLCHXM/Program.cs
@@ -15,7 +15,7 @@ namespace BTL_LTTQ_QLCHXM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new MainForm());
         }
 
 
