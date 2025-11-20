3fd413817e50839c088bcbb24f61561c7d192198
thanh-dat1f <vntd1f@gmail.com>
2025-11-20 12:13:43 +0700

    done dashboard

---
+++ BLL/DashboardBLL.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class DashboardBLL
+    {
+        private readonly DashboardDAL _dal = new DashboardDAL();
+
+        public DashboardDTO GetStats() => _dal.GetStats();
+        public DataTable LayXeMoiNhap(int topN = 8) => _dal.LayXeMoiNhap(topN);
+    }
+}
---
+++ DTO/DashboardDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO
+{
+    public class DashboardDTO
+    {
+        public int XeSanSang { get; set; }
+        public int GiaoDichBanHomNay { get; set; }
+        public int ThueDangHoatDong { get; set; }
+        public decimal DoanhThuThangNay { get; set; }
+    }
+}
